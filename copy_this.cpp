e8f14f45f4f77d4ce7fb60f812b39015645deabf
Vijay Jadhav <vijayjadhav008@gmail.com>
2016-11-22 19:06:01 +0530

    first program and starting point code

---
+++ copy_this.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <limits>
+#include <functional>
+
+#define L(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define LE(i,l,u) for(int (i)=(int)(l);(i)<=(int)(u);++(i))
+#define LL(i,l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i))
+#define each(it,object) for(auto (it)=(object).begin();(it)!=(object).end();++(it))
+#define pb(item) push_back(item)
+#define mp(item1,item2) make_pair((item1),(item2))
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+#define ull unsigned long long int
+#define ll long long int
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pair<int,int> > vpii;
+typedef vector<int> vi;
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
